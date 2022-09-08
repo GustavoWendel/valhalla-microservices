1e54ee2afe8c0c3f452dcfdc3efb1d3c3a23f69f
Gustavo Wendel <gustavoborges@sisconsultoria.com.br>
2022-09-08 12:28:46 -0300

    Created Valhalla App and Eureka Server

---
+++ valhallaapp/src/main/java/br/com/valhalla/valhallaapp/ValhallaappApplication.java
@@ -0,0 +1,13 @@
+package br.com.valhalla.valhallaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValhallaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValhallaappApplication.class, args);
+	}
+
+}
---
+++ valhallaapp/src/test/java/br/com/valhalla/valhallaapp/ValhallaappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valhalla.valhallaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValhallaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
